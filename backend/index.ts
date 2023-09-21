4f77142d61fef13c2605389d0f66fe8ddd5351aa
Robin Delbaere <hello@rdelbaere.fr>
2023-09-21 23:13:58 +0200

    Add socket connection

---
+++ backend/index.ts
@@ -0,0 +1,11 @@
+import { Server } from "socket.io";
+
+const io = new Server(3000, {
+    cors: {
+        origin: '*',
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log(socket);
+});
